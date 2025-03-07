02c6cb456dc0bb6c6d7717feabc3b327577efbb1
Anthony Sandhar-Cruz <123418080+AnthonySandhar-Cruz@users.noreply.github.com>
2025-03-07 11:36:35 -0500

    Fix beat indicator scaling issues

--- Assets/Scripts/BeatRoller.cs
+++ Assets/Scripts/BeatRoller.cs
@@ -1,35 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BeatRoller : MonoBehaviour
 {
-    public AudioSource audio;
+    public float firstBeatOffset = 0.55f;
+    new public AudioSource audio;
+    public Canvas parentCanvas;
     public GameObject ball;
     public GameObject wholeBeat;
     public GameObject halfBeat;
     public float bpm = 130f;
     private float halfDt; // Time between half beats
     private float halfBeatDist; // Distance between half beats
-    private float halfBeatSpeed; // Speed half beats travel
-    private int rangeStart = -9; // Left edge of UI
-    private int rangeEnd = 9; // Right edge of UI
+    private float halfBeatSpeed;
+    private int rangeStart = - 1920 / 2;
+    private int rangeEnd = 1920 / 2;
     private int rangeLen;
-    private int numBeatDivisions; // Number of indicators on screen
+    private int numBeatDivisions = 16; // Number of indicators on screen
     private GameObject[] allBeats; // Array to hold copied sprites
-    private Vector3 ballOrigin; // Bottom of ball bounce
-    private Vector3 ballTarget; // Peak of ball bounce
+    private Vector2 ballOrigin; // Bottom of ball bounce
+    private Vector2 ballTarget; // Peak of ball bounce
     private float[] timeElapsed; // Helps to calculate how much beat sprites should shift each frame
     private int iter = 0; // Help to make random golden notes
-    private SpriteRenderer wholeBeatSR; // Used to check if a copy is a whole beat
+    private Color white;
+    private Color yellow;
 
     void Start()
     {
-        ballOrigin = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.y);
-        ballTarget = ballOrigin + new Vector3(0,1,0);
+        // Set up default and golden note colors
+        white = new Color(1f, 1f, 1f);
+        white.a = 1;
+        yellow = new Color(1f, 1f, 0f);
+        yellow.a = 1;
+
+        ballOrigin = ball.GetComponent<Image>().GetComponent<RectTransform>().anchoredPosition;
+        ballTarget = ballOrigin + new Vector2(0, 150);
 
         rangeLen = rangeEnd - rangeStart;
-        numBeatDivisions = rangeLen;
-
-        wholeBeatSR = wholeBeat.GetComponent<SpriteRenderer>();
 
         wholeBeat.SetActive(false);
         halfBeat.SetActive(false);
@@ -39,50 +46,57 @@ public class BeatRoller : MonoBehaviour
         timeElapsed = new float[numBeatDivisions];
 
         halfDt = (60f / bpm) / 2f;
-        halfBeatDist = (rangeEnd - rangeStart) / numBeatDivisions;
+        halfBeatDist = (rangeEnd - rangeStart) / (numBeatDivisions);
         halfBeatSpeed = halfBeatDist / halfDt;
 
-        // Clone whole beat and half beat sprites to fill range
-        // TO-DO: fix first beat offset, maybe check once audio time has passed offset before starting the ping pong in update, and add offset to Instantiate x position
-        for (int i = 0; i < rangeLen; i++) {
-            if (i % 2 != 0) {
-                allBeats[i] = Instantiate(wholeBeat, new Vector3(rangeStart+i*halfBeatDist, wholeBeat.transform.position.y, -1), new Quaternion(0, 0, 0, 0));
+       for (int i = 0; i < numBeatDivisions; i++) {
+            float xCalc = rangeStart + i * halfBeatDist;
+            if (i % 2 == 0) {
+                allBeats[i] = Instantiate(wholeBeat);
             } else {
-                allBeats[i] = Instantiate(halfBeat, new Vector3(rangeStart+i*halfBeatDist, halfBeat.transform.position.y, -1), new Quaternion(0, 0, 0, 0));
+                allBeats[i] = Instantiate(halfBeat);
             }
+
+            allBeats[i].transform.SetParent(parentCanvas.transform, false);
+            Image img = allBeats[i].GetComponent<Image>();
+            RectTransform rt = img.GetComponent<RectTransform>();
+            rt.anchoredPosition = new Vector2(xCalc, halfBeat.GetComponent<Image>().GetComponent<RectTransform>().anchoredPosition.y);
+
             allBeats[i].SetActive(true);
         }
 
-        audio.Play(); // Start music after setup
+        audio.Play();
     }
 
     void Update()
     {
-        // Move ball up and down with a period of 1 beat
-        ball.transform.position = Vector3.Lerp(ballOrigin, ballTarget, Mathf.PingPong(Time.time * halfBeatSpeed, 1));
+        if (audio.time >= firstBeatOffset) {
 
-        // Move each half and whole beat sprite to the left
-        for (int i = 0; i < rangeLen; i++) {
-            GameObject currBeat = allBeats[i];
+            ball.GetComponent<Image>().GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(ballOrigin, ballTarget, Mathf.PingPong(Time.time / (halfDt), 1));
 
-            // Wrap to right edge of range if sprite hits left edge
-            if (currBeat.transform.position.x <= rangeStart) {
-                // Reset color of any golden notes
-                SpriteRenderer sr = currBeat.GetComponent<SpriteRenderer>();
-                sr.color = Color.white;
+            // Shift beats left
+            for (int i = 0; i < numBeatDivisions; i++) {
+                GameObject currBeat = allBeats[i];
 
-                currBeat.transform.position = new Vector3(rangeEnd, halfBeat.transform.position.y, -2);
+                Image currIMG = currBeat.GetComponent<Image>();
+                RectTransform currRT = currIMG.GetComponent<RectTransform>();
 
-                // Insert random golden notes on whole beats
-                if (iter % 17 == 0 && sr.sprite == wholeBeatSR.sprite) {
-                    sr.color = Color.yellow;
+                // Wrap to end of range
+                if (currRT.anchoredPosition.x <= rangeStart) {
+                    currIMG.color = white;
+
+                    currRT.anchoredPosition = new Vector2(rangeEnd, halfBeat.GetComponent<Image>().GetComponent<RectTransform>().anchoredPosition.y);
+
+                    // Insert random golden notes
+                    if (iter % 17 == 0 && currBeat.CompareTag("WholeBeat")) {
+                        currIMG.color = yellow;
+                    }
                 }
-            }
 
-            // Move beats left, calculated using speed and difference between current time and time in previous frame
-            currBeat.transform.position = new Vector3(currBeat.transform.position.x - (halfBeatSpeed*(Time.time - timeElapsed[i])), wholeBeat.transform.position.y, -2);
-            timeElapsed[i] = Time.time;
+                currRT.anchoredPosition = new Vector2(currRT.anchoredPosition.x - (halfBeatSpeed*(Time.time - timeElapsed[i])), wholeBeat.GetComponent<Image>().GetComponent<RectTransform>().anchoredPosition.y);
+                timeElapsed[i] = Time.time;
+            }
+            iter++;
         }
-        iter++;
     }
 }
